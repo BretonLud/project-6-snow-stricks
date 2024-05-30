fb9a6bbef6bd652f9c32661ecddaf0837e27fcd2
BretonLud <godneo@hotmail.fr>
2024-05-30 12:38:39 +0200

    Add save and remove methods to GroupRepository
    Unused commented methods were removed from the GroupRepository and replaced with save and remove methods. These methods were added in order to allow for persistent saving and deletion of Group entities.

--- src/Repository/GroupRepository.php
+++ src/Repository/GroupRepository.php
@@ -15,29 +15,20 @@ class GroupRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Group::class);
     }
-
-    //    /**
-    //     * @return Group[] Returns an array of Group objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('g')
-    //            ->andWhere('g.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('g.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
-
-    //    public function findOneBySomeField($value): ?Group
-    //    {
-    //        return $this->createQueryBuilder('g')
-    //            ->andWhere('g.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
+    public function save(Group $group, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($group);
+        if ($flush)
+        {
+            $this->getEntityManager()->flush();
+        }
+    }
+    
+    public function remove(Group $group, bool $flush = false): void
+    {
+        $this->getEntityManager()->remove($group);
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
 }
