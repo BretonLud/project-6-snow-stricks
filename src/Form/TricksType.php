02a2ceefe0a05e7b5d649e20d642a81c2652a2e7
BretonLud <godneo@hotmail.fr>
2024-05-30 12:41:54 +0200

    Add TricksType Form
    The commit introduces a new Form Type `TricksType`. This form has fields title, content, and category with the title and content as text fields and category is a special field that uses EntityType to produce a dropdown of 'Group' entities. The form type is configured to be used with the 'Tricks' entity.

---
+++ src/Form/TricksType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Group;
+use App\Entity\Tricks;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TricksType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class, [
+                'required' => true,
+            ])
+            ->add('content', TextareaType::class, [
+                'required' => true,
+            ])
+            ->add('category', EntityType::class, [
+            'class' => Group::class,
+            'required' => true,
+            'autocomplete' => true,
+            'placeholder' => 'Select a group',
+        ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Tricks::class,
+        ]);
+    }
+}
