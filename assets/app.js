3f682fa86ff5a9c0518c6ab09fe334e00a867b79
BretonLud <godneo@hotmail.fr>
2024-05-30 12:36:12 +0200

    Add AutocompleteBundle and update related assets
    Added the AutocompleteBundle to the project config. Also updated 'app.js', 'importmap.php', and 'controllers.json' files to import necessary styles and register the bundle. Also introduced the 'tom-select' library for improved select functionality.

--- assets/app.js
+++ assets/app.js
@@ -6,4 +6,6 @@ import './bootstrap.js';
  * which should already be in your base.html.twig.
  */
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.min.css'
+import 'tom-select/dist/css/tom-select.default.css'
 import './styles/app.css';
