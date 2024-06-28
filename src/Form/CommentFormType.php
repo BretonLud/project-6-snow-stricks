fbe2453feaf4e53e9e42089696ea4ae03502ad30
BretonLud <godneo@hotmail.fr>
2024-06-28 10:31:38 +0200

    Update CommentFormType fields
    Unused use statements and an unnecessary field were removed from CommentFormType.php. Additionally, the label for the "content" field was modified from "Content" to "Message".

--- src/Form/CommentFormType.php
+++ src/Form/CommentFormType.php
@@ -3,9 +3,6 @@
 namespace App\Form;
 
 use App\Entity\Comment;
-use App\Entity\Tricks;
-use App\Entity\User;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -18,8 +15,7 @@ class CommentFormType extends AbstractType
         $builder
             ->add('content', TextareaType::class, [
                 'required' => true,
-                'label' => 'Content',
-                'rows' => 10
+                'label' => 'Message',
             ])
         ;
     }
