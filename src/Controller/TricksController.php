c84b4dcb4a9d9dbdea58daa3f681f972155ed242
BretonLud <godneo@hotmail.fr>
2024-06-28 10:28:19 +0200

    Add delete and edit functions for tricks and comments
    The TricksController has been updated to include functions for deleting and editing tricks and comments in the system. This includes the validation of CSRF tokens for these actions. The MapEntity attribute has also been applied to the edit and show methods in the TricksController to make mapping between entities and routes easier and more efficient.

--- src/Controller/TricksController.php
+++ src/Controller/TricksController.php
@@ -9,8 +9,11 @@ use App\Form\TricksType;
 use App\Service\CommentService;
 use App\Service\PicturesUploaderService;
 use App\Service\TricksService;
+use Doctrine\ORM\Mapping\Entity;
 use Exception;
+use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -63,7 +66,7 @@ class TricksController extends AbstractController
      */
     #[Route('/edit/{slug}', name: 'edit', methods: ['GET', 'POST'])]
     #[IsGranted("ROLE_USER")]
-    public function edit(Request $request, Tricks $tricks): Response
+    public function edit(Request $request,#[MapEntity(mapping: ['slug' => 'slug'])] Tricks $tricks): Response
     {
         $form = $this->createForm(TricksType::class, $tricks);
         $form->handleRequest($request);
@@ -82,7 +85,7 @@ class TricksController extends AbstractController
     }
     
     #[Route('/show/{slug}', name: 'show', methods: ['GET', 'POST'])]
-    public function show(Tricks $tricks, Request $request): Response
+    public function show(#[MapEntity(mapping: ['slug' => 'slug'])] Tricks $tricks, Request $request): Response
     {
         $comment = new Comment();
         $form = $this->createForm(CommentFormType::class, $comment);
@@ -93,11 +96,80 @@ class TricksController extends AbstractController
             $comment->setTricks($tricks);
             $this->commentService->save($comment);
             $this->addFlash('success', 'Comment saved.');
+            return $this->redirectToRoute('app_tricks_show', ['slug' => $tricks->getSlug()]);
         }
         
         return $this->render('tricks/show.html.twig', [
             'tricks' => $tricks,
             'form' => $form
         ]);
+    }
+    
+    /**
+     * @throws Exception
+     */
+    #[Route('/delete/{slug}', name: 'delete', methods: ['DELETE'])]
+    #[isGranted("ROLE_USER")]
+    public function delete(#[MapEntity(mapping: ['slug' => 'slug'])] Tricks $tricks, Request $request): Response
+    {
+        if ($this->isCsrfTokenValid('delete-tricks-' . $tricks->getSlug(), $request->get('_token'))) {
+            
+            $this->tricksService->remove($tricks);
+            
+            $this->addFlash('success', 'Tricks deleted');
+        } else {
+            $this->addFlash('error', 'Cannot delete this tricks ');
+        }
+        
+        return $this->redirectToRoute('app_tricks_index');
+    }
+    
+    #[Route('/show/{slug}/comment/edit/{id}', name: 'editComment', methods: ['GET', 'POST'])]
+    #[IsGranted("COMMENT_EDIT", "comment")]
+    public function editComment(
+        #[MapEntity(mapping: ['slug' => 'slug'])]Tricks $tricks,
+        #[MapEntity(mapping: ['id' => 'id'])]Comment $comment,
+        Request $request
+    ): Response|RedirectResponse
+    {
+        $form = $this->createForm(CommentFormType::class, $comment, [
+            'action' => $this->generateUrl('app_tricks_editComment', ['slug' => $tricks->getSlug(), 'id' => $comment->getId()]),
+        ]);
+        
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            
+            $this->commentService->save($comment);
+            $this->addFlash('success', 'Comment edited.');
+            return $this->redirectToRoute('app_tricks_show', ['slug' => $tricks->getSlug()]);
+            
+        }
+        
+        return $this->render('comment/_form.html.twig', [
+            'form' => $form,
+            'tricks' => $tricks,
+            'comment' => $comment
+        ]);
+        
+    }
+    
+    #[Route('/show/{slug}/comment/delete/{id}', name: 'deleteComment', methods: ['DELETE'])]
+    #[IsGranted("COMMENT_EDIT", "comment")]
+    public function deleteComment(
+        #[MapEntity(mapping: ['slug' => 'slug'])]Tricks $tricks,
+        #[MapEntity(mapping: ['id' => 'id'])]Comment $comment,
+        Request $request
+    ): Response|RedirectResponse
+    {
+        if ($this->isCsrfTokenValid('delete-comment-' . $comment->getId(), $request->get('_token'))) {
+            
+            $this->commentService->remove($comment);
+            
+            $this->addFlash('success', 'Comment deleted');
+        } else {
+            $this->addFlash('error', 'Cannot delete this comment ');
+        }
+        
+        return $this->redirectToRoute('app_tricks_show', ['slug' => $tricks->getSlug()]);
     }
 }
