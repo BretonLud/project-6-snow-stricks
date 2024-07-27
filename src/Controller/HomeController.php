9de0dfa9682a3712319cafc4bfbba8c0bd13a540
BretonLud <godneo@hotmail.fr>
2024-07-27 18:03:37 +0200

    Add tricks fetching and pagination in HomeController
    Integrate `TricksService` to fetch and display tricks with pagination. Introduce `loadMoreTricks` route to facilitate loading additional tricks dynamically.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Service\TricksService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,9 +10,28 @@ use Symfony\Component\Routing\Attribute\Route;
 #[Route(name: 'app_home_')]
 class HomeController extends AbstractController
 {
+    public function __construct(private readonly TricksService $tricksService)
+    {
+    }
+    
     #[Route('/', name: 'index', methods: ['GET'])]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig');
+        $tricksLength = $this->tricksService->count();
+        $tricks = $this->tricksService->findByLimit(6,0, ['createdAt' => 'DESC']);
+        return $this->render('home/index.html.twig', [
+            'tricks' => $tricks,
+            'tricksLength' => $tricksLength,
+        ]);
+    }
+    
+    #[Route('/load-more-tricks/{offset}', name: 'load_more_tricks', methods: ['GET'])]
+    public function loadMoreTricks(int $offset): Response
+    {
+        $tricks = $this->tricksService->findByLimit(6, $offset, ['createdAt' => 'DESC']);
+        
+        return $this->render('_partials/_tricks.html.twig', [
+            'tricks' => $tricks,
+        ]);
     }
 }
