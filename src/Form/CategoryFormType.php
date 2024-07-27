6276735f4f696cb8823edb5a615e4c2042f29875
BretonLud <godneo@hotmail.fr>
2024-07-27 18:04:16 +0200

    Add additional field configuration to CategoryFormType
    Modified the 'name' field in CategoryFormType to use TextType with specific attributes. This includes setting the field as required and adding CSS classes for styling.

--- src/Form/CategoryFormType.php
+++ src/Form/CategoryFormType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Category;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,7 +13,12 @@ class CategoryFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
+            ->add('name', TextType::class, [
+                'required' => true,
+                'row_attr' => [
+                    'class' => 'form-floating mb-3',
+                ],
+            ])
         ;
     }
 
