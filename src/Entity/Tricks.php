11a886e1612b60a46f7c8932c77f43b90451a800
BretonLud <godneo@hotmail.fr>
2024-08-03 20:01:55 +0200

    Add HTML content sanitization in Tricks entity
    Integrated HTMLPurifier to sanitize the content in the Tricks entity before saving to the database. This ensures that any HTML in the content field is cleaned to prevent XSS attacks.

--- src/Entity/Tricks.php
+++ src/Entity/Tricks.php
@@ -1,257 +1,268 @@
 <?php
-
-namespace App\Entity;
-
-use App\Entity\Trait\DateTrait;
-use App\Repository\TricksRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
-use Doctrine\ORM\Mapping as ORM;
-use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
-use Symfony\Component\String\Slugger\AsciiSlugger;
-use Symfony\Component\Validator\Constraints as Assert;
-
-#[ORM\Entity(repositoryClass: TricksRepository::class)]
-#[ORM\HasLifecycleCallbacks]
-#[UniqueEntity(fields: ['title'], message: 'Trick already exists.')]
-class Tricks
-{
-    use DateTrait;
     
-    #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column]
-    private ?int $id = null;
-
-    #[ORM\Column(length: 255)]
-    #[Assert\NotBlank()]
-    #[Assert\NotNull()]
-    private ?string $title = null;
-
-    #[ORM\Column(type: "text", length: 10000)]
-    #[Assert\NotBlank()]
-    #[Assert\NotNull()]
-    #[Assert\Type('string')]
-    private ?string $content = null;
-
-    #[ORM\Column(length: 255)]
-    #[Assert\Type('string')]
-    private ?string $summary = null;
-
-    #[ORM\ManyToOne(inversedBy: 'tricks')]
-    #[ORM\JoinColumn(nullable: false)]
-    private ?User $user = null;
-
-    #[ORM\ManyToOne(inversedBy: 'tricks')]
-    #[ORM\JoinColumn(nullable: false)]
-    #[Assert\NotNull]
-    #[Assert\NotBlank]
-    private ?Category $category = null;
-
-    #[ORM\Column(length: 255)]
-    #[Assert\Type('string')]
-    private ?string $slug = null;
-
-    /**
-     * @var Collection<int, Picture>
-     */
-    #[ORM\OneToMany(targetEntity: Picture::class, mappedBy: 'tricks', cascade: ['persist','remove'], orphanRemoval: true)]
-    private Collection $pictures;
-
-    /**
-     * @var Collection<int, Video>
-     */
-    #[ORM\OneToMany(targetEntity: Video::class, mappedBy: 'tricks', cascade: ['persist','remove'], orphanRemoval: true)]
-    private Collection $videos;
-
-    /**
-     * @var Collection<int, Comment>
-     */
-    #[ORM\OneToMany(targetEntity: Comment::class, mappedBy: 'tricks', orphanRemoval: true)]
-    private Collection $comments;
+    namespace App\Entity;
     
-    public function __construct()
-    {
-        $this->createdAt = new \DateTimeImmutable();
-        $this->updatedAt = new \DateTimeImmutable();
-        $this->pictures = new ArrayCollection();
-        $this->videos = new ArrayCollection();
-        $this->comments = new ArrayCollection();
-    }
+    use App\Entity\Trait\DateTrait;
+    use App\Repository\TricksRepository;
+    use Doctrine\Common\Collections\ArrayCollection;
+    use Doctrine\Common\Collections\Collection;
+    use Doctrine\ORM\Mapping as ORM;
+    use HTMLPurifier;
+    use HTMLPurifier_Config;
+    use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+    use Symfony\Component\String\Slugger\AsciiSlugger;
+    use Symfony\Component\Validator\Constraints as Assert;
     
-    public function getId(): ?int
+    #[ORM\Entity(repositoryClass: TricksRepository::class)]
+    #[ORM\HasLifecycleCallbacks]
+    #[UniqueEntity(fields: ['title'], message: 'Trick already exists.')]
+    class Tricks
     {
-        return $this->id;
-    }
-
-    public function getTitle(): ?string
-    {
-        return $this->title;
-    }
-
-    public function setTitle(string $title): static
-    {
-        $this->title = $title;
-        $this->updateSlug();
-
-        return $this;
-    }
-
-    public function getContent(): ?string
-    {
-        return $this->content;
-    }
-
-    public function setContent(string $content): static
-    {
-        $this->content = $content;
-
-        return $this;
-    }
-
-    public function getSummary(): ?string
-    {
-        return $this->summary;
-    }
-    
-    #[ORM\PrePersist]
-    #[ORM\PreUpdate]
-    public function setSummary(): static
-    {
-        if ($this->content) {
-            $this->summary = substr($this->content, 0, 255);
+        use DateTrait;
+        
+        #[ORM\Id]
+        #[ORM\GeneratedValue]
+        #[ORM\Column]
+        private ?int $id = null;
+        
+        #[ORM\Column(length: 255)]
+        #[Assert\NotBlank()]
+        #[Assert\NotNull()]
+        private ?string $title = null;
+        
+        #[ORM\Column(type: "text", length: 10000)]
+        #[Assert\NotBlank()]
+        #[Assert\NotNull()]
+        #[Assert\Type('string')]
+        private ?string $content = null;
+        
+        #[ORM\Column(length: 255)]
+        #[Assert\Type('string')]
+        private ?string $summary = null;
+        
+        #[ORM\ManyToOne(inversedBy: 'tricks')]
+        #[ORM\JoinColumn(nullable: false)]
+        private ?User $user = null;
+        
+        #[ORM\ManyToOne(inversedBy: 'tricks')]
+        #[ORM\JoinColumn(nullable: false)]
+        #[Assert\NotNull]
+        #[Assert\NotBlank]
+        private ?Category $category = null;
+        
+        #[ORM\Column(length: 255)]
+        #[Assert\Type('string')]
+        private ?string $slug = null;
+        
+        /**
+         * @var Collection<int, Picture>
+         */
+        #[ORM\OneToMany(targetEntity: Picture::class, mappedBy: 'tricks', cascade: ['persist', 'remove'], orphanRemoval: true)]
+        private Collection $pictures;
+        
+        /**
+         * @var Collection<int, Video>
+         */
+        #[ORM\OneToMany(targetEntity: Video::class, mappedBy: 'tricks', cascade: ['persist', 'remove'], orphanRemoval: true)]
+        private Collection $videos;
+        
+        /**
+         * @var Collection<int, Comment>
+         */
+        #[ORM\OneToMany(targetEntity: Comment::class, mappedBy: 'tricks', orphanRemoval: true)]
+        private Collection $comments;
+        
+        public function __construct()
+        {
+            $this->createdAt = new \DateTimeImmutable();
+            $this->updatedAt = new \DateTimeImmutable();
+            $this->pictures = new ArrayCollection();
+            $this->videos = new ArrayCollection();
+            $this->comments = new ArrayCollection();
         }
         
-        return $this;
-    }
-
-    public function getUser(): ?User
-    {
-        return $this->user;
-    }
-
-    public function setUser(?User $user): static
-    {
-        $this->user = $user;
-
-        return $this;
-    }
-
-    public function getCategory(): ?Category
-    {
-        return $this->category;
-    }
-
-    public function setCategory(?Category $category): static
-    {
-        $this->category = $category;
-
-        return $this;
-    }
-
-    public function getSlug(): ?string
-    {
-        return $this->slug;
-    }
-    
-    public function updateSlug(): void
-    {
-        $slugger = new AsciiSlugger();
-        $this->slug = $slugger->slug($this->title);
-    }
-    
-    public function __toString(): string
-    {
-        return $this->title;
-    }
-
-    /**
-     * @return Collection<int, Picture>
-     */
-    public function getPictures(): Collection
-    {
-        return $this->pictures;
-    }
-
-    public function addPicture(Picture $picture): static
-    {
-        if (!$this->pictures->contains($picture)) {
-            $this->pictures->add($picture);
-            $picture->setTricks($this);
+        public function getId(): ?int
+        {
+            return $this->id;
         }
-
-        return $this;
-    }
-
-    public function removePicture(Picture $picture): static
-    {
-        if ($this->pictures->removeElement($picture)) {
-            // set the owning side to null (unless already changed)
-            if ($picture->getTricks() === $this) {
-                $picture->setTricks(null);
+        
+        public function getTitle(): ?string
+        {
+            return $this->title;
+        }
+        
+        public function setTitle(string $title): static
+        {
+            $this->title = $title;
+            $this->updateSlug();
+            
+            return $this;
+        }
+        
+        public function updateSlug(): void
+        {
+            $slugger = new AsciiSlugger();
+            $this->slug = $slugger->slug($this->title);
+        }
+        
+        public function getContent(): ?string
+        {
+            return $this->content;
+        }
+        
+        public function setContent(string $content): static
+        {
+            $this->content = $this->sanitizeContent($content);
+            
+            return $this;
+        }
+        
+        private function sanitizeContent(string $content): string
+        {
+            $config = HTMLPurifier_Config::createDefault();
+            
+            $purifier = new HTMLPurifier($config);
+            
+            return $purifier->purify($content);
+        }
+        
+        public function getSummary(): ?string
+        {
+            return $this->summary;
+        }
+        
+        #[ORM\PrePersist]
+        #[ORM\PreUpdate]
+        public function setSummary(): static
+        {
+            if ($this->content) {
+                $this->summary = substr($this->content, 0, 255);
             }
+            
+            return $this;
         }
-
-        return $this;
-    }
-
-    /**
-     * @return Collection<int, Video>
-     */
-    public function getVideos(): Collection
-    {
-        return $this->videos;
-    }
-
-    public function addVideo(Video $video): static
-    {
-        if (!$this->videos->contains($video)) {
-            $this->videos->add($video);
-            $video->setTricks($this);
+        
+        public function getUser(): ?User
+        {
+            return $this->user;
         }
-
-        return $this;
-    }
-
-    public function removeVideo(Video $video): static
-    {
-        if ($this->videos->removeElement($video)) {
-            // set the owning side to null (unless already changed)
-            if ($video->getTricks() === $this) {
-                $video->setTricks(null);
+        
+        public function setUser(?User $user): static
+        {
+            $this->user = $user;
+            
+            return $this;
+        }
+        
+        public function getCategory(): ?Category
+        {
+            return $this->category;
+        }
+        
+        public function setCategory(?Category $category): static
+        {
+            $this->category = $category;
+            
+            return $this;
+        }
+        
+        public function getSlug(): ?string
+        {
+            return $this->slug;
+        }
+        
+        public function __toString(): string
+        {
+            return $this->title;
+        }
+        
+        /**
+         * @return Collection<int, Picture>
+         */
+        public function getPictures(): Collection
+        {
+            return $this->pictures;
+        }
+        
+        public function addPicture(Picture $picture): static
+        {
+            if (!$this->pictures->contains($picture)) {
+                $this->pictures->add($picture);
+                $picture->setTricks($this);
             }
+            
+            return $this;
         }
-
-        return $this;
-    }
-
-    /**
-     * @return Collection<int, Comment>
-     */
-    public function getComments(): Collection
-    {
-        return $this->comments;
-    }
-
-    public function addComment(Comment $comment): static
-    {
-        if (!$this->comments->contains($comment)) {
-            $this->comments->add($comment);
-            $comment->setTricks($this);
-        }
-
-        return $this;
-    }
-
-    public function removeComment(Comment $comment): static
-    {
-        if ($this->comments->removeElement($comment)) {
-            // set the owning side to null (unless already changed)
-            if ($comment->getTricks() === $this) {
-                $comment->setTricks(null);
+        
+        public function removePicture(Picture $picture): static
+        {
+            if ($this->pictures->removeElement($picture)) {
+                // set the owning side to null (unless already changed)
+                if ($picture->getTricks() === $this) {
+                    $picture->setTricks(null);
+                }
             }
+            
+            return $this;
         }
-
-        return $this;
+        
+        /**
+         * @return Collection<int, Video>
+         */
+        public function getVideos(): Collection
+        {
+            return $this->videos;
+        }
+        
+        public function addVideo(Video $video): static
+        {
+            if (!$this->videos->contains($video)) {
+                $this->videos->add($video);
+                $video->setTricks($this);
+            }
+            
+            return $this;
+        }
+        
+        public function removeVideo(Video $video): static
+        {
+            if ($this->videos->removeElement($video)) {
+                // set the owning side to null (unless already changed)
+                if ($video->getTricks() === $this) {
+                    $video->setTricks(null);
+                }
+            }
+            
+            return $this;
+        }
+        
+        /**
+         * @return Collection<int, Comment>
+         */
+        public function getComments(): Collection
+        {
+            return $this->comments;
+        }
+        
+        public function addComment(Comment $comment): static
+        {
+            if (!$this->comments->contains($comment)) {
+                $this->comments->add($comment);
+                $comment->setTricks($this);
+            }
+            
+            return $this;
+        }
+        
+        public function removeComment(Comment $comment): static
+        {
+            if ($this->comments->removeElement($comment)) {
+                // set the owning side to null (unless already changed)
+                if ($comment->getTricks() === $this) {
+                    $comment->setTricks(null);
+                }
+            }
+            
+            return $this;
+        }
     }
-}
