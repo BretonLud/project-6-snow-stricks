f12cecf2653e66aa1f4d5db25c5e294b694ffea0
BretonLud <godneo@hotmail.fr>
2024-08-17 15:12:47 +0200

    Format code and remove 'link' from CKEditor toolbar
    Improved code readability by standardizing spacing and indentation. Also, removed the 'link' button from the CKEditor toolbar configuration to streamline the editor's toolbar options.

--- assets/controllers/ckeditor_controller.js
+++ assets/controllers/ckeditor_controller.js
@@ -1,6 +1,7 @@
 import {Controller} from "@hotwired/stimulus";
 import {BlockQuote, Bold, ClassicEditor, CodeBlock, Essentials, Font, Heading, Italic, Link, List, Paragraph, Undo, WordCount} from "ckeditor5";
 import "@ckeditor/ckeditor5-build-classic/build/translations/fr"
+
 export default class extends Controller {
 
     static targets = ["editor"];
@@ -11,15 +12,14 @@ export default class extends Controller {
 
     initializeEditor() {
         const maxCharacters = 10000;
-        const container = document.querySelector( '.update' );
-        const progressCircle = document.querySelector( '.update__chart__circle' );
-        const charactersBox = document.querySelector( '.update__chart__characters' );
-        const circleCircumference = Math.floor( 2 * Math.PI * progressCircle.getAttribute( 'r' ) );
+        const container = document.querySelector('.update');
+        const progressCircle = document.querySelector('.update__chart__circle');
+        const charactersBox = document.querySelector('.update__chart__characters');
+        const circleCircumference = Math.floor(2 * Math.PI * progressCircle.getAttribute('r'));
 
         let language = 'en';
         let placeholder = "Trick content"
-        if (navigator.language === 'fr' || navigator.language === 'fr-FR')
-        {
+        if (navigator.language === 'fr' || navigator.language === 'fr-FR') {
             language = 'fr'
             placeholder = "Description du trick"
         }
@@ -31,7 +31,7 @@ export default class extends Controller {
             .create(textarea, {
                 language: language,
                 placeholder: placeholder,
-                plugins: [ WordCount, Paragraph, Font, Bold, Essentials, Italic, Undo, Heading, Link, BlockQuote, CodeBlock, List],
+                plugins: [WordCount, Paragraph, Font, Bold, Essentials, Italic, Undo, Heading, Link, BlockQuote, CodeBlock, List],
                 toolbar: {
                     items: [
                         'undo', 'redo',
@@ -42,7 +42,7 @@ export default class extends Controller {
                         '|',
                         'bold', 'italic',
                         '|',
-                        'link', 'blockQuote', 'codeBlock',
+                        'blockQuote', 'codeBlock',
                         '|',
                         'bulletedList', 'numberedList'
                     ],
@@ -53,27 +53,27 @@ export default class extends Controller {
                         const charactersProgress = stats.characters / maxCharacters * circleCircumference;
                         const isLimitExceeded = stats.characters > maxCharacters;
                         const isCloseToLimit = !isLimitExceeded && stats.characters > maxCharacters * .8;
-                        const circleDashArray = Math.min( charactersProgress, circleCircumference );
+                        const circleDashArray = Math.min(charactersProgress, circleCircumference);
 
                         // Set the stroke of the circle to show how many characters were typed.
-                        progressCircle.setAttribute( 'stroke-dasharray', `${ circleDashArray },${ circleCircumference }` );
+                        progressCircle.setAttribute('stroke-dasharray', `${circleDashArray},${circleCircumference}`);
 
                         // Display the number of characters in the progress chart. When the limit is exceeded,
                         // display how many characters should be removed.
-                        if ( isLimitExceeded ) {
-                            charactersBox.textContent = `-${ stats.characters - maxCharacters }`;
+                        if (isLimitExceeded) {
+                            charactersBox.textContent = `-${stats.characters - maxCharacters}`;
                         } else {
                             charactersBox.textContent = stats.characters;
                         }
 
                         // If the content length is close to the character limit, add a CSS class to warn the user.
-                        container.classList.toggle( 'update__limit-close', isCloseToLimit );
+                        container.classList.toggle('update__limit-close', isCloseToLimit);
 
                         // If the character limit is exceeded, add a CSS class that makes the content's background red.
-                        container.classList.toggle( 'update__limit-exceeded', isLimitExceeded );
+                        container.classList.toggle('update__limit-exceeded', isLimitExceeded);
 
                         // If the character limit is exceeded, disable the send button.
-                        btn.toggleAttribute( 'disabled', isLimitExceeded );
+                        btn.toggleAttribute('disabled', isLimitExceeded);
                     }
                 }
             })
@@ -101,7 +101,7 @@ export default class extends Controller {
                     form.requestSubmit()
                 })
             }).catch(error => {
-                console.error(error);
+            console.error(error);
         })
 
     }
