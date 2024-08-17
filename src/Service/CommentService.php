21bb24768139774bba92f0154bf16b422b01e24a
BretonLud <godneo@hotmail.fr>
2024-08-17 14:15:44 +0200

    Add pagination support to CommentService
    Introduced a new method, `getCommentsPaginated`, in `CommentService` that retrieves paginated comments for a given trick. This enhances the service's functionality by enabling efficient comment retrieval in chunks, improving performance for large data sets.

--- src/Service/CommentService.php
+++ src/Service/CommentService.php
@@ -1,22 +1,29 @@
 <?php
-
-namespace App\Service;
-
-use App\Entity\Comment;
-use App\Repository\CommentRepository;
-
-readonly class CommentService
-{
-    public function __construct(private CommentRepository $commentRepository)
-    {}
     
-    public function save(Comment $comment): void
-    {
-        $this->commentRepository->save($comment, true);
-    }
+    namespace App\Service;
     
-    public function remove(Comment $comment): void
+    use App\Entity\Comment;
+    use App\Entity\Tricks;
+    use App\Repository\CommentRepository;
+    
+    readonly class CommentService
     {
-        $this->commentRepository->remove($comment, true);
+        public function __construct(private CommentRepository $commentRepository)
+        {
+        }
+        
+        public function save(Comment $comment): void
+        {
+            $this->commentRepository->save($comment, true);
+        }
+        
+        public function remove(Comment $comment): void
+        {
+            $this->commentRepository->remove($comment, true);
+        }
+        
+        public function getCommentsPaginated(Tricks $trick, int $page): array
+        {
+            return $this->commentRepository->getCommentsPaginated($trick, $page);
+        }
     }
-}
