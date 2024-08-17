06bf57dedb917f0303152b05f4018878082e74de
BretonLud <godneo@hotmail.fr>
2024-08-17 14:15:31 +0200

    Add pagination to CommentRepository
    Implemented a new method `getCommentsPaginated` to fetch comments with pagination based on a given trick and page number. This includes calculating the total number of pages and ensuring the query adjusts to the correct set of results for the requested page.

--- src/Repository/CommentRepository.php
+++ src/Repository/CommentRepository.php
@@ -1,36 +1,74 @@
 <?php
-
-namespace App\Repository;
-
-use App\Entity\Comment;
-use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\Persistence\ManagerRegistry;
-
-/**
- * @extends ServiceEntityRepository<Comment>
- */
-class CommentRepository extends ServiceEntityRepository
-{
-    public function __construct(ManagerRegistry $registry)
-    {
-        parent::__construct($registry, Comment::class);
-    }
     
-    public function save(Comment $comment, bool $flush = false): void
+    namespace App\Repository;
+    
+    use App\Entity\Comment;
+    use App\Entity\Tricks;
+    use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+    use Doctrine\ORM\Tools\Pagination\Paginator;
+    use Doctrine\Persistence\ManagerRegistry;
+    
+    /**
+     * @extends ServiceEntityRepository<Comment>
+     */
+    class CommentRepository extends ServiceEntityRepository
     {
-        $this->getEntityManager()->persist($comment);
-        
-        if ($flush)
+        public function __construct(ManagerRegistry $registry)
         {
-            $this->getEntityManager()->flush();
+            parent::__construct($registry, Comment::class);
+        }
+        
+        public function save(Comment $comment, bool $flush = false): void
+        {
+            $this->getEntityManager()->persist($comment);
+            
+            if ($flush) {
+                $this->getEntityManager()->flush();
+            }
+        }
+        
+        public function remove(Comment $comment, bool $flush = false): void
+        {
+            $this->getEntityManager()->remove($comment);
+            if ($flush) {
+                $this->getEntityManager()->flush();
+            }
+        }
+        
+        public function getCommentsPaginated(Tricks $trick, int $page): array
+        {
+            $result = [];
+            
+            $query = $this->createQueryBuilder('c')
+                ->andWhere('c.tricks = :trick')
+                ->setParameter('trick', $trick)
+                ->orderBy('c.createdAt', 'DESC')
+                ->setFirstResult(($page - 1) * 10)
+                ->setMaxResults(10)
+                ->getQuery();
+            
+            $paginator = new Paginator($query);
+            $pages = 1;
+            
+            if ($paginator->count() > 0) {
+                $pages = ceil($paginator->count() / 10);
+                
+                if ($page > $pages) {
+                    $query->setFirstResult(($pages - 1) * 10);
+                    $page = $pages;
+                }
+            }
+            
+            $data = $paginator->getQuery()->getResult();
+            
+            if (empty($data)) {
+                return $result;
+            }
+            
+            $result['pages'] = $pages;
+            $result['comments'] = $data;
+            $result['page'] = $page;
+            
+            return $result;
         }
     }
-    
-    public function remove(Comment $comment, bool $flush = false): void
-    {
-        $this->getEntityManager()->remove($comment);
-        if ($flush) {
-            $this->getEntityManager()->flush();
-        }
-    }
-}
